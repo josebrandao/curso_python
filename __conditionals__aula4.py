4e2ebe3a65ddef4759684ffb8f344b2f0a2c0adb
jose.sbrandao <jose.sbrandao@gmail.com>
2019-08-04 21:49:54 -0300

    included comments

--- __conditionals__aula4.py
+++ __conditionals__aula4.py
@@ -4,12 +4,12 @@
 
 def main():
   x, y = 100, 100
-  
+
   # conditional statements let you use "a if C else b"
   st = "x is less than y" if (x < y) else "x is greater than or equal to y"
   print st
 
-  # conditional flow uses if, elif, else  
+  # conditional flow uses if, elif, else
   if(x < y):
     st= "x is less than y"
   elif (x == y):
@@ -20,6 +20,7 @@ def main():
 
   # Python does not have support for higher-order conditionals
   # like "switch-case" in other languages
-  
+
 if __name__ == "__main__":
   main()
+  #test function
